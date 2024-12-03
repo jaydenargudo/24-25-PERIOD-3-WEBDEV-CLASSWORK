6ada6d55f2bb2d3129998dee2da8e0689f61b770
Jayden Argudo <jayarg26@bergen.org>
2024-12-03 10:20:43 -0500

    12/3/24

--- Unit3_BTRE/btre/settings.py
+++ Unit3_BTRE/btre/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = {
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
-}
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
